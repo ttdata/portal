853134817dc1be57adf16837b0b5b456e7586447
ren shan <ren.shan@unisys.com>
2020-05-10 14:13:55 -0400

    updated ttdataTestingScript

--- APIs/ttdataTestingScript.py
+++ APIs/ttdataTestingScript.py
@@ -1,15 +1,20 @@
 ## Purpose: provide a generic service for testing API calls
 ## Author : Simon Li
 ## Date   : May 5, 2020
-##
+## Modif  : Ren Shan
+## Last updated: 5/10/2020
 ## hostname refer settings.json in .vscode folder
 ##
 ## Usage:  
 ## import TTDataService
 ## ttdata = TTDataService("108.48.52.183")
-    
+from pprint import pprint 
+import pandas as pd
+import numpy as np
+import os
 import requests
 import json
+import time    
 from termcolor import colored
 
 class TTDataService:
@@ -95,6 +100,64 @@ class TTDataService:
                 response = requests.post(url, data = payload)    
 
         return (response.status_code, TTDataService.JSONParseIfPossible(response.text))
+
+def transform_names(df):
+    '''
+       need to have standard names
+    '''
+    df.columns = [x.lower() for x in df.columns]
+    df.rename(columns={'province/state': 'province_state',
+                   'admin2'            : 'county',
+                   'country/region'    : 'country_region',
+                   'last update'       : 'last_update',
+                   "long_":          "longitude",
+                   "long":          "longitude",
+                   "lat":           "latitude",
+                   "lat_":           "latitude",
+                  }, inplace=True)
+    
+    if ("combined_key" in df.columns):
+       df.drop(columns=['combined_key'], inplace=True)
+       
+    keycolumn = "country_region|province_state|date"
+
+    if ("county" in df.columns):
+        keycolumn = "country_region|province_state|county|date"
+  
+    return (df, keycolumn)
+
+def covid19data_to_ttdata(did, source_file, URL, data_date, blocksize, waitsec):
+    print("processing: ", source_file)
+    df = pd.read_csv(source_file, dtype = {'FIPS': str, 'ZIP': str, "ZIP_CODE": str})
+    df, key = transform_names(df)
+    #errorCount = 0
+    
+    totalsize = df.shape[0]
+    batches =[list(range(i, min(i+blocksize, totalsize))) for i in range(0,totalsize,blocksize) ]
+    
+    for aBatch in batches:
+        json_data_list = []
+        for i in aBatch:
+            row = df.iloc[i]
+            #row_as_dic = row.to_dict()
+            device_data = {"date":data_date }
+            device_data.update(json.loads(row.to_json()))
+            json_data_list.append(device_data)
+        
+        print("\nFor batch from: ", aBatch[0], ", to:", aBatch[-1])
+        payload = {
+             "data_type": 2, 
+             "device_id": did,
+             "key" : key,
+             "device_data": json_data_list                
+              }
+        #print(payload)
+        response = requests.post(URL, json = payload)
+        print(response.elapsed.total_seconds())
+        print(response.status_code)
+        print(response.text)
+        time.sleep(waitsec)
+
 def check_status(stage,response):    
     if (response[0] == 200):
         print(stage, ": pass")
@@ -102,14 +165,28 @@ def check_status(stage,response):
         print(stage, ": failed")
         #TODO send sms to the system admin
 
+def check_covid19(host, did):
+    github_root = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports"
+
+    file = github_root + "/05-09-2020" + ".csv"
+    ts = time.time()
+    covid19data_to_ttdata(did, source_file= file, 
+                            URL = host + "/uploaddata", 
+                            data_date = "2020-03-25", 
+                            blocksize = 500,
+                            waitsec = 1)
+    te = time.time()
+    print('%s  %2.2f sec' % ("uploaddata", (te - ts) ))
+
 if __name__ == '__main__':
-    host_url = 'http://192.168.1.196:7061'
+    host_url = 'http://ixinbuy.com:7061'
+    #host_url = 'http://192.168.1.196:7061'
     print(host_url)
     ttdata = TTDataService()
     ttdata.url = host_url
     # Register
     print("== /register ==")
-    payload = '{"device_id": "test-ttdata-101"}'
+    payload = '{"device_id": "test-ttdata-1"}'
     response = ttdata.post("/register", payload)
     print(response)
     check_status("register", response)
@@ -118,12 +195,12 @@ if __name__ == '__main__':
     payload = {
                 "data_type": 2, 
                 "device_id": "test-ttdata-1", 
-                "key": "date|province|county|city",
+                "key": "date|province_state|county|country_region",
                 "device_data": [{
                                     "date": "2020-05-06", 
-                                    "province": "ON",
-                                    "country": "Canada", 
-                                    "city": "Toronto",
+                                    "province_state": "ON",
+                                    "country_region": "Canada", 
+                                    "county": "Toronto",
                                     "last_update": "2020-05-03T10:43:02",
                                     "confirmed":370,
                                     "deaths": 84,
@@ -136,9 +213,15 @@ if __name__ == '__main__':
     response = ttdata.post("/uploaddata", payload)
     #print(json.dumps(payload))
     print(response)
-    check_status("register", response)
+    check_status("uploaddata", response)
     # Get shared data
     print("\n== /getshareddata ==")
     response = ttdata.get("/getshareddata?data_type={0}&device_id={1}".format(2, "test-ttdata-1"))
     print(response)
-    check_status("register", response)
+    check_status("getshareddata", response)
+    
+    print("\n== /covid19 ==")
+    #check_covid19(host_url, "test-ttdata-1")
+    
+
+    
