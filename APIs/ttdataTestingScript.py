4c78d05e8faf13052a8f15b51ebf4246b0698d75
m <office@cabinjohnconsulting.com>
2020-06-09 09:46:43 -0400

    update

--- APIs/ttdataTestingScript.py
+++ APIs/ttdataTestingScript.py
@@ -228,7 +228,7 @@ def check_reg_upload_getdata(did):
                 "device_id": did, 
                 "key": "date|province_state|county|country_region",
                 "device_data": [{
-                                    "date": "2020-05-06", 
+                                    "date": "2020-05-07", 
                                     "province_state": "ON",
                                     "country_region": "Canada", 
                                     "county": "Toronto",
@@ -277,7 +277,7 @@ if __name__ == '__main__':
     #   0 means for none
     #   any number larger than totalsize of the data / blocksize means for all
     # 
-    check_covid19_upload(host_url, device_id, 200, 9999)
+    check_covid19_upload(host_url, device_id, 500, 9999)
 
     
     
