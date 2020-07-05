48977a1a0b506b7a5339cb5d6750b038c14ad37a
YY0919 <37122917+YY0919@users.noreply.github.com>
2020-07-05 00:37:27 -0400

    initial commit

---
+++ dash/ttdata_dash.py
@@ -0,0 +1,210 @@
+import dash
+# import dash_auth
+import dash_core_components as dcc
+import dash_html_components as html
+import dash_bootstrap_components as dbc
+import dash_auth
+import dash_table
+from dash.dependencies import Input, Output
+import pandas as pd
+import plotly.express as px
+import requests
+import json
+import time 
+
+# df = pd.read_csv("/Users/yangyuan/Documents/ttdata/dash/dash_sample.csv")
+# DEVICEID = 'test_20200625'
+# URL = 'http://108.48.52.183:7061'
+# d_type = 2
+
+# # def validate(URL, d_type):
+
+# URL_upload = URL + '/uploaddata'
+# URL_uploadbatch = URL + '/uploaddata'
+# URL_get = URL + '/getshareddata'
+
+# #register testing
+# payload = {
+# "device_id":DEVICEID,
+# "category": "test"
+# }
+
+# #Passing payload as dict
+# response_register= requests.post(URL_register, data = payload, timeout = 5)
+# register_result = json.loads(response_register.text)
+
+# #893f0bb8571bc3b51257cea17b88b206457864b3ef98bdf68a45f1bf907fe012
+# appkey = None
+# if register_result["data"]:
+#      appkey = register_result["data"]
+# print(appkey)
+
+# # upload testing 
+# payload = {"data_type": d_type, 
+#             "device_id": DEVICEID, 
+#             "key": "date|province_state|county|country_region", 
+#             "device_data": [{
+#                                 "date": "2020-05-08", 
+#                                 "province_state": "ON",
+#                                 "country_region": "Canada", 
+#                                 "county": "Toronto",
+#                                 "last_update": "2020-05-03T10:43:02",
+#                                 "confirmed":370,
+#                                 "deaths": 84,
+#                                 "recovered": 14,
+#                                 "latitude":43.6532,
+#                                 "longitude": 79.3832
+#                     }]   
+#         }
+#     #Passing payload as dict
+# response_upload = requests.post(URL_upload, 
+#     json = payload, 
+#     timeout = 5, 
+#     headers = {"appkey" : appkey})
+# upload_result = json.loads(response_upload.text)
+
+# print(upload_result['message'])
+
+
+# time.sleep(5)
+# # get data testing
+# query = {"data_type": d_type, "device_id": DEVICEID}
+# response_get = requests.get(URL_get, params=query, timeout = 5, headers = {"appkey" : appkey})
+# get_result = json.loads(response_get.text)
+
+# print(get_result['message'])
+
+
+# Program to build the dashbaord
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+
+# Keep this out of source code repository - save in a file or a database
+VALID_USERNAME_PASSWORD_PAIRS = {
+    'yang': 'yang'
+}
+
+auth = dash_auth.BasicAuth(
+    app,
+    VALID_USERNAME_PASSWORD_PAIRS
+)
+
+# validate('http://108.48.52.183:7061',2)
+
+app.layout = html.Div([
+    html.H1(children='TTDATA API Test'),
+
+    html.Div(children='''
+        A web application to test the TTDATA servers
+    '''),
+    #register input
+    html.Label(["Selected API",
+    dcc.Dropdown(
+                id='API_selection',
+                options=[{'label': 'RS serve: http://ttdata.life:7061', 'value': 'http://ttdata.life:7061'},
+                {'label': 'ZN serve: http://112.17.170.154:7061', 'value': 'http://112.17.170.154:7061'},
+                {'label': 'IXINBUY serve: http://ixinbuy.com:7061', 'value': 'http://ixinbuy.com:7061'},
+                ],
+                value='http://ttdata.life:7061'
+            )]),
+
+    html.H1(children='Register API'),
+    dbc.FormGroup([dbc.Label("Device ID"), dbc.Input(id = "Input_device_id", placeholder="Input the device id here...", type="text")]),
+    dbc.FormGroup([dbc.Label("Category"), dbc.Input(id = "Input_category", placeholder="Input the category here...", type="text")]),
+    dbc.Button("Register", id = "reg_submit_button", color="primary", className="mr-1"),
+    html.Br(),
+    html.P(id="reg_output"),
+
+    #Upload input
+    html.H1(children='Uploaddata API'),
+    
+    dcc.Dropdown(
+                id='dtype_selection',
+                options=[{'label': 'Type 1', 'value': 1},
+                {'label': 'Type 2', 'value': 2},
+                {'label': 'Type 1001', 'value': 3},
+                ],
+                value='1'),
+    dbc.FormGroup([dbc.Label("Key field"), dbc.Input(id = "Input_keyfield", placeholder="Input the key field of payload", type="text")]),
+    dbc.FormGroup([dbc.Label("Payload"), dbc.Textarea(id = "Input_payload", placeholder="Input the payload", style = {'width': '30%',
+        'height': '600%'},)]),
+    html.Br(),
+    dbc.Button("Uplaod", id = "upload_submit_button", color="primary", className="mr-1"),
+    html.P(id="upload_output"),
+])
+# register call function
+@app.callback(
+        Output("reg_output", "children"), 
+        [Input("API_selection", "value"),
+        Input("reg_submit_button", "n_clicks"),
+        Input("Input_device_id", "value"),
+        Input("Input_category", "value")])
+def send_register_request(URL, n_clicks, DEVICEID, category):
+    URL_register = URL + '/register'
+    register_result = " "
+    register_text = None
+    if n_clicks:
+        #register testing
+        payload = {
+        "device_id":DEVICEID,
+        "category": category
+        }
+        #Passing payload as dict
+        response_register= requests.post(URL_register, data = payload, timeout = 5)
+        register_text = response_register.text
+        register_result = json.loads(register_text)
+        global appkey
+        appkey = None
+        if register_result["data"]:
+            appkey = register_result["data"]
+    return register_text
+
+#upload data 
+@app.callback(
+    Output("upload_output", "children"), 
+    [Input("API_selection", "value"),
+    Input("dtype_selection", "value"),
+    Input("Input_device_id", "value"),
+    Input("upload_submit_button", "n_clicks"),
+    Input("Input_keyfield", "value"),
+    Input("Input_payload", "value")]
+)
+def send_upload_request(URL, dtype, DEVICEID, n_clicks, keyfield, payload):
+    URL_upload = URL + '/uploaddata'
+    upload_result_mesg = None
+    if n_clicks:
+        payload = {"data_type": dtype, 
+                    "device_id": DEVICEID, 
+                    "key": "date", 
+                    "device_data": payload   
+                }
+        #Passing payload as dict
+        response_upload = requests.post(URL_upload, 
+            json = payload, 
+            timeout = 5, 
+            headers = {"appkey" : appkey})
+        upload_result = json.loads(response_upload.text)
+        upload_result_mesg = upload_result['message']
+    return upload_result_mesg
+  
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
+
+
+
+# [{
+#     "date": "2020-05-08", 
+#     "province_state": "ON",
+#     "country_region": "Canada", 
+#     "county": "Toronto",
+#     "last_update": "2020-05-03T10:43:02",
+#     "confirmed":370,
+#     "deaths": 84,
+#     "recovered": 14,
+#     "latitude":43.6532,
+#     "longitude": 79.3832
+# }]
